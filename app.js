8e722ab1ef054b607c087b4aec414a1b4b5329b9
drygnet <github@dryg.net>
2024-01-09 20:58:26 +0100

    Update index.html with Text Artisan details and GitHub link

---
+++ app.js
@@ -0,0 +1,4 @@
+// event listeners for the buttons
+document.getElementById("try-artisan").addEventListener("click", function() {
+  window.location.href = "https://chat.openai.com/g/g-QYYzwpKAN-text-artisan";
+});
